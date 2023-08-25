00ba24a2466c15080c85b6bf721ee4de30cb3746
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-08-26 00:28:08 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,32 +1,20 @@
-import './App.css';
-import React from "react"
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import { BrowserRouter as Main, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Header from './components/Header';
 import About from './components/About';
+import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Error from './components/Error';
+import './App.css';
 
 function App() {
   return (
-    < >
-    <Main>
-      <Navbar />
-      <Routes>
-<Route exact path='/home' element= {<Home/>}/>
-<Route exact path='/about' element= {<About/>}/>
-<Route exact path='/contact' element= {<Contact/>}/>
-<Route  path='/*' element= {<Error/>}/>
+    <div className="App">
+      <Header />
+      <About />
+      <Projects />
 
-
-      </Routes>
-    </Main>
-      
-    </>
-
-  )
+      <Contact />
+    </div>
+  );
 }
-
-
 
 export default App;
